4867f1fba1abec4fd978ae72955dabb45686fa8c
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-07-13 10:25:17 -0300

    chore: mock concurrent processing working

--- internal/questions/questions.go
+++ internal/questions/questions.go
@@ -3,6 +3,7 @@ package questions
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/KaueSabinoSRV17/lesson_automation/internal/studeoapi"
 )
@@ -18,13 +19,6 @@ type Question struct {
 	Alternatives []Alternative `json:"alternativaList"`
 }
 
-func GetQuestionsFromQuestionary(api studeoapi.Client) []Question {
-	path := "/objeto-ensino-api-controller/api/questao/shortname/2023_EGRAD_ADSIS6GA-52_EGRAD_NGER100_053/questionario/227751"
-	var questions []Question
-	api.Get(path, nil, &questions)
-	return questions
-}
-
 func (q *Question) FormatQuestionAndAlternatives() string {
 	question := fmt.Sprintf("Tenho uma pergunta e algumas alternativas. Com base nelas, me diga qual o ID da alternativa correta %s", q.Description)
 	var alternatives []string
@@ -34,4 +28,35 @@ func (q *Question) FormatQuestionAndAlternatives() string {
 	}
 	fullString := strings.Join(alternatives, "\n")
 	return fullString
+}
+
+func GetQuestionsFromQuestionary(api studeoapi.Client) []Question {
+	path := "/objeto-ensino-api-controller/api/questao/shortname/2023_EGRAD_ADSIS6GA-52_EGRAD_NGER100_053/questionario/227751"
+	var questions []Question
+	api.Get(path, nil, &questions)
+	return questions
+}
+
+func GetAnswersFromQuestionsList(questions []Question) []int {
+	answers := make([]int, len(questions))
+	channel := make(chan int)
+	for index, question := range questions {
+		go func(q Question, i int) {
+			answer := MockGPTQuestion(q, i)
+			channel <- answer
+		}(question, index)
+	}
+
+	for index := range questions {
+		answer := <-channel
+		answers[index] = answer
+	}
+
+	return answers
+}
+
+func MockGPTQuestion(question Question, index int) int {
+	time.Sleep(time.Second * 2)
+	fmt.Printf("\nAnswer to %v is DeezNuttzz. Number %v\n", question.Description, index)
+	return 0
 }
--- internal/questions/questions_test.go
+++ internal/questions/questions_test.go
@@ -7,8 +7,6 @@ import (
 	"github.com/KaueSabinoSRV17/lesson_automation/internal/studeoapi"
 )
 
-// Questionary ID: 227751
-
 func TestGetQuestionsFromQuestionary(t *testing.T) {
 	api, err := studeoapi.AuthenticatedInstance()
 	if err != nil {
@@ -31,4 +29,29 @@ func TestFormatQuestionAndAlternatives(t *testing.T) {
 	if !strings.Contains(fullString, "4") {
 		t.Error("Should Contain 4, an Alternative")
 	}
+}
+
+func TestGetAnswersFromQuestionsList(t *testing.T) {
+	mockQuestion := Question{
+		ID:          1,
+		Description: "How much is 2 plus 2?",
+		Alternatives: []Alternative{
+			{ID: 1, Description: "2"},
+			{ID: 2, Description: "4"},
+		},
+	}
+
+	questions := fill(mockQuestion, 10)
+	answers := GetAnswersFromQuestionsList(questions)
+	if len(answers) == 0 {
+		t.Error("Should not be empty")
+	}
+}
+
+func fill(question Question, numberOfQuestions int) []Question {
+	questions := make([]Question, numberOfQuestions)
+	for i := 0; i < numberOfQuestions; i++ {
+		questions[i] = question
+	}
+	return questions
 }
